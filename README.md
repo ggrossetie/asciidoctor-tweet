9f909045ae400c2d0859651a636fa7271d6cb1fe
Guillaume Grossetie <ggrossetie@gmail.com>
2019-03-17 15:15:11 +0100

    resolves #1 #2 tweet block, default theme and fallback as a quote
    - Add a default theme (using a DocinfoProcessor)
    - Return a quote when the backend is not html5
    - tweet can be defined as a block

--- README.md
+++ README.md
@@ -10,20 +10,32 @@ An extension for [Asciidoctor.js](https://github.com/asciidoctor/asciidoctor.js)
 
 ## Usage
 
-In your document, use the `tweet` macro with the name of the feature:
+In your document, use the `tweet` macro with a tweet id:
 
 ```
 tweet::988973671065600000[]
 ```
 
-Register the extension before converting your document:
+**TIP:** The tweet id can be found in a tweet URL.
+For instance in https://twitter.com/GretaThunberg/status/1041369960436703232, the tweet id is `104136996043670323`.
+
+You can also use a `tweet` block as you would use a *quote* block:
+
+```
+[tweet,Greta Thunberg,@GretaThunberg,16 sept. 2018,avatar-url=https://pbs.twimg.com/profile_images/1103201826449604608/c0t5HuM-_400x400.jpg,retweet-count=11 k,favorite-count=30 k]
+____
+Fridays for future. The school strike continues! #climatestrike #klimatstrejk #FridaysForFuture
+____
+```
+
+Then, register the extension before converting your document:
 
 ```js
 const asciidoctor = require('asciidoctor.js')()
 const tweetExtension = require('asciidoctor-tweet')
 const registry = tweetExtension.register(asciidoctor.Extensions.create())
 
-asciidoctor.convertFile('sample.adoc', {extension_registry: registry})
+asciidoctor.convertFile('sample.adoc', { extension_registry: registry })
 ```
 
 **IMPORTANT**:
@@ -38,5 +50,3 @@ Once you have a Twitter application, you will need to pass the consumer key and 
 ## Rendering
 
 ![](rendering.jpeg)
-
-**NOTE**: You can use a `docinfo` file to add custom stylesheet. See the `examples` directory.
--- test/test.js
+++ test/test.js
@@ -7,10 +7,46 @@ const cheerio = require('cheerio')
 const registry = asciidoctorTweet.register(asciidoctor.Extensions.create())
 
 describe('Asciidoctor tweet extension', () => {
-  it('should display a tweet', () => {
-    const html = asciidoctor.convert('tweet::988973671065600000[]', {extension_registry: registry})
+  it('should display a tweet using a block macro', () => {
+    const html = asciidoctor.convert('tweet::988973671065600000[]', { extension_registry: registry })
     const result = cheerio.load(html)
     expect(result('.tweet')).to.have.lengthOf(1)
     expect(result('.tweet-text').text()).to.equal('Asciidoctor 1.5.7 is going to feature much more accurate line tracking. While there are still edge cases, the numbers will be correct in most documents.')
+  })
+
+  it('should display a tweet using a block', () => {
+    const html = asciidoctor.convert(`[tweet,Dan Allen,@mojavelinux,25 April 2018,avatar-url=https://pbs.twimg.com/profile_images/1054511920223526912/6ul47E16.jpg,retweet-count=3,favorite-count=1]
+____
+Asciidoctor 1.5.7 is going to feature much more accurate line tracking. While there are still edge cases, the numbers will be correct in most documents.
+____`, { extension_registry: registry })
+    const result = cheerio.load(html)
+    expect(result('.tweet')).to.have.lengthOf(1)
+    expect(result('.tweet-text').text()).to.equal('Asciidoctor 1.5.7 is going to feature much more accurate line tracking. While there are still edge cases, the numbers will be correct in most documents.')
+  })
+
+  it('should display a tweet using a block and add stylesheet', () => {
+    const html = asciidoctor.convert(`[tweet,Greta Thunberg,@GretaThunberg,16 sept. 2018,avatar-url=https://pbs.twimg.com/profile_images/1103201826449604608/c0t5HuM-_400x400.jpg,retweet-count=11 k,favorite-count=30 k]
+____
+Fridays for future. The school strike continues! #climatestrike #klimatstrejk #FridaysForFuture
+____`, { safe: 'safe', header_footer: true, extension_registry: registry })
+    const result = cheerio.load(html)
+    expect(result('.tweet')).to.have.lengthOf(1)
+    expect(result('.tweet-text').text()).to.equal('Fridays for future. The school strike continues! #climatestrike #klimatstrejk #FridaysForFuture')
+    expect(result('.tweet-style')).to.have.lengthOf(1)
+  })
+
+  it('should use a default avatar image', () => {
+    const html = asciidoctor.convert(`[tweet,Thomas Parisot,@oncletom,6 déc. 2018,retweet-count=22,favorite-count=19]
+____
+🌟 OK, le livre « Node.js • Apprendre par la pratique » débarque en librairie aujourd'hui ! De quoi faire un bon tour de #NodeJS v10 et #npm v6.
+
+🧭 Où le trouver ? https://www.placedeslibraires.fr/livre/9782212139938
+ℹ️ Le reste est sur https://oncletom.io/node.js/
+
+🥁 *merci* à… ⤵
+____`, { safe: 'safe', header_footer: true, extension_registry: registry })
+    const result = cheerio.load(html)
+    expect(result('.tweet')).to.have.lengthOf(1)
+    expect(result('.tweet-author-avatar > img').attr('src')).to.equal('https://abs.twimg.com/sticky/default_profile_images/default_profile.png')
   })
 })
